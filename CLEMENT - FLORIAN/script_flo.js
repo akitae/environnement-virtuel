9599adeeaa78880a0a2ecd6e1fb8365b79cfd800
HyperNovax <florian.lephore@outlook.com>
2018-06-07 12:04:54 +0200

    Point of sphere

--- CLEMENT - FLORIAN/script_flo.js
+++ CLEMENT - FLORIAN/script_flo.js
@@ -1,6 +1,6 @@
 // Variables globals
 var renderer, scene, camera, sphGeo, material, sphere;
-var texture;
+var texture, texture_bleu;
 var controls;
 var isDragging;
 var previousMousePosition = {
@@ -12,7 +12,7 @@ init();
 animate();
 
 function init () {
-    renderer = new THREE.CanvasRenderer();
+    renderer = new THREE.WebGLRenderer();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -21,22 +21,61 @@ function init () {
     scene = new THREE.Scene();
 
     camera  = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(0, 0, 4);
+    camera.position.set(0, 0, 5);
     scene.add(camera);
 
     controls = new THREE.OrbitControls(camera);
 
     texture = new THREE.TextureLoader().load(
+        "texture.jpg"
+    );
+
+    texture_bleu = new THREE.TextureLoader().load(
         "texture_bleu.jpg"
     );
 
+
+
     sphGeo = new THREE.SphereGeometry(1 ,100 ,100);
     material = [
+        new THREE.MeshBasicMaterial({ map: texture_bleu }),
         new THREE.MeshBasicMaterial({ map: texture })
     ];
 
     sphere = new THREE.Mesh(sphGeo, material);
+    sphere.position.y = 0;
+    sphere.position.x = 0;
+    sphere.position.z = 0;
     scene.add(sphere);
+
+    var latitude = 0;
+    var longitude = 0;
+    var radius = 1;
+
+    var littleSphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var littleSphereGeo = new THREE.SphereGeometry(0.002);
+
+    while (longitude < 360) {
+
+        var phi = (latitude / 180) * Math.PI;
+        var theta = (longitude / 180) * Math.PI;
+
+        var gridPosition = {
+            x: radius * Math.cos(theta) * Math.sin(phi),
+            y: radius * Math.sin(theta) * Math.sin(phi),
+            z: radius * Math.cos(phi)
+        };
+
+        var sphero = new THREE.Mesh(littleSphereGeo, littleSphereMaterial);
+        sphero.position.set(gridPosition.x, gridPosition.y, gridPosition.z);
+        scene.add(sphero);
+
+        latitude += 5;
+        if (latitude > 360) {
+            latitude = 0;
+            longitude += 5;
+        }
+    }
 
 
     // Ajout des événements
